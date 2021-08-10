a2b21fefe238031b130ca7d661e9f45edad7b016
Bibletoon <alex8.ivanov@gmail.com>
2021-08-10 15:12:17 +0300

    Feat: add models

---
+++ ProductsBase.Data/Models/Page.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ProductsBase.Data.Models
+{
+    public class Page<T>
+    {
+        public static int DefaultSize = 10;
+        private const int MaxPageSize = 500;
+        private int _pageSize;
+        public int PageSize 
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? value : MaxPageSize;
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<T> Items { get; set; }
+
+        public Page()
+        {
+            Items = new List<T>();
+        }
+    }
+}
---
+++ ProductsBase.Data/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductsBase.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public UnitOfMeasurement UnitOfMeasurement { get; set; }
+        public int QuantityInPackage  { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
